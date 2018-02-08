7730175fa44c33b2d0972b7c6b7d5279e2c753fb
jlaville <j.laville@qwant.com>
2018-02-08 10:18:13 +0100

    WIP front with vue

---
+++ src/vues/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <hierarchy>
+
+  </hierarchy>
+</template>
+
+<script>
+  import hierarchy from "vue-loader!./hierarchy.vue";
+  export default {
+    name: "app",
+    components : {
+      hierarchy
+    }
+  }
+</script>
---
+++ src/vues/hierarchy.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hierarchy" v-if="hierarchy">
+    <h3>{{ hierarchy.name }}</h3>
+    <table>
+      <tr v-for="property in hierarchy.properties">
+        <td>{{ property.name }}</td>
+        <td>{{ property.value }}</td>
+      </tr>
+    </table>
+
+    <h3>Parent hierarchy</h3>
+    <h3>Child hierarchy</h3>
+    <h3>Other linked zone</h3>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "hierarchy",
+    data : () => {
+      return {
+        hierarchy : null
+      }
+    },
+    created : () => {
+      listen('show_hierarchy', (id) => {
+
+      })
+    }
+  }
+</script>

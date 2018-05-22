a277d00daa4616669266cb0e88fe83b5d8565381
Antoine D <antoine.desbordes@gmail.com>
2018-05-22 16:49:50 +0200

    make it possible to be highly available
    now we fill a table in an `import` schema, generate tiles from this in
    a `import_cosmogony` directory.
    
    when this is finished we move as a quite atomic operation the table to
    the `public` schema and the tiles to the repository watched by the t-rex
    server

--- run_explorer.sh
+++ run_explorer.sh
@@ -11,9 +11,13 @@ docker-compose up -d
 
 echo "Import to Postgis will start..."
 sleep 10 # Waiting for postgres
-docker-compose run -v "`dirname $(realpath $1)`:/tmp/volume" cosmogony-importer ./import.py /tmp/volume/`basename $1`
+docker-compose run -v "`dirname $(realpath $1)`:/tmp/volume" cosmogony-importer ./import.py import_data /tmp/volume/`basename $1`
 
 echo "Generating tiles..."
-docker-compose exec cosmogony-tiles /usr/bin/t_rex generate -c /config.toml --minzoom 0 --maxzoom 6 --overwrite true --progress true
+docker-compose exec cosmogony-tiles /usr/bin/t_rex generate -c /config_generate.toml --minzoom 0 --maxzoom 6 --overwrite true --progress true
+
+# publish the data so they can be used (without any downtime)
+docker-compose run cosmogony-importer ./import.py publish
+docker-compose exec cosmogony-tiles /publish_tiles.sh
 
 sensible-browser "http://localhost:8585/#2.5/32/0"

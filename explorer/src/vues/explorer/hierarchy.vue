a3d66e3d3841f5c74be6172a70ad7557d429a3f3
jlaville <j.laville@qwant.com>
2018-08-16 19:28:32 +0200

    remove console.log

--- explorer/src/vues/explorer/hierarchy.vue
+++ explorer/src/vues/explorer/hierarchy.vue
@@ -60,7 +60,6 @@
     },
     created : async function () {
       listen('update_hierarchy', async (hierarchyId) => {
-        console.log('>',hierarchyId)
         this.hierarchy = await  Hierarchy.load(hierarchyId)
         pushState({hierarchyId : hierarchyId})
         fire('select_hierarchy', hierarchyId)
